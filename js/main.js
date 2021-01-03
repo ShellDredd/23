dbb0adfb1d47f82a7a826e7cbdafbe8197fb319a
Broken__System <70167960+ShellDredd@users.noreply.github.com>
2021-01-03 19:48:32 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="texto1">Freedom Users</i>',
+		'<i class="texto2">Hacking Society</i>',
+		'<i class="texto3">Practice with machines</i>',
+		'<i class="texto1">Exclusive Machines</i>',
+		'<i class="texto2">knowledge Library</i>',
+		'<i class="texto3">Community and Forum</i>',
+		'<i class="texto1">Real and Thematic Environments</i>',
+		'<i class="texto2">Sector 23</i>',
+	],	
+	
+	typeSpeed: 50, 
+	startDelay: 300,
+	backSpeed: 50,
+	smartBackspace: true,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|',
+	contentType: 'html',
+});
